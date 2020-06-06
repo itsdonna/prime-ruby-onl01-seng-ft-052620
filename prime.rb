dde3eb6caf06099d477b83500c50bfb315794399
Donna Torres <todonnatorres@icloud.com>
2020-06-06 15:46:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 require 'benchmark'
 require 'bigdecimal/math'
 
-def prime?(interger)
-  i = 2
-  
-  
-  
-end # Add  code here!
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
